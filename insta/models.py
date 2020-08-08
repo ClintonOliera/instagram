214c2e4dfbca441adb373bff3ecec63f9715d6a6
clintonoliera <clintonoliera391@gmail.com>
2020-08-08 11:45:50 +0300

    added comment model to the app

--- insta/models.py
+++ insta/models.py
@@ -61,4 +61,16 @@ class Post(models.Model):
         return self.likes.count()
 
     def __str__(self):
-        return f'{self.user.name} Post'        
+        return f'{self.user.name} Post'
+        
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.name} Post'
+
+    class Meta:
+        ordering = ["-pk"]                
