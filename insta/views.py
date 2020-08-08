d971645507c778217aa8d63c268459e0467c52c8
clintonoliera <clintonoliera391@gmail.com>
2020-08-08 13:02:55 +0300

    this commit will add view function for signup request

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm, PostForm, CommentForm
+from django.contrib.auth import login, authenticate
+from .models import Post, Comment, Profile, Follow
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.views.generic import RedirectView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
