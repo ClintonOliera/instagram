efcd3aaef5f2d1c082b79d2e43254b7627429eaf
clintonoliera <clintonoliera391@gmail.com>
2020-08-10 18:57:05 +0300

    updated profile models

--- insta/models.py
+++ insta/models.py
@@ -6,8 +6,8 @@ from django.dispatch import receiver
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture = models.ImageField(upload_to='images/', default='default.png')
-    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    profile_picture = models.ImageField(upload_to='images/')
+    bio = models.TextField(max_length=200, default="My Bio", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
 
